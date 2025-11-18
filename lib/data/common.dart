55585b7d407574a856fdbcb9fa325ee38b04ff22
duzhaoquan <duzhaoquan123@163.com>
2025-11-18 19:00:10 +0800

    添加iOS启动图

--- lib/data/common.dart
+++ lib/data/common.dart
@@ -4,16 +4,20 @@
 // 竖版插屏：106981、
 // 激励视频：111366
 //
-//调试-IOS-APPID：20825 、调试-IOS-广告位ID：开屏：104833 、大图原生：106043 、小图原生：106063 、插屏：107249、激励视频：111367
+//调试-IOS-APPID：20825 、调试-IOS-广告位ID：开屏：104833 、大图原生：106043 、自渲染原生：106063 、插屏：107249、激励视频：111367
 //
 //这是配套使用的测试ID。先用这套ID调试，调试好给我们发个测试包看看，没问题的话我们会提供正式ID替换
 //
 //http://sdkdoc.beizi.biz/#/
 
-const appId = "20826";
-const splashSpaceId = '104835';
-const interstitialSpaceId = '106981';
-const nativeSpaceId = '106064';
-const unifiedSpaceId = '106064';
-const rewardVideoSpaceId = '111366';
-const timeOut = 5000;//广告请求超时时长，建议5000毫秒,该参数单位为ms
+import 'dart:io';
+
+var appId = Platform.isAndroid ? "20826" : "20825";
+var splashSpaceId = Platform.isAndroid ? '104835' : "104833";
+var interstitialSpaceId = Platform.isAndroid ? '106981' : "107249";
+var nativeSpaceId = Platform.isAndroid ? '106064' : "106043";
+var unifiedSpaceId = Platform.isAndroid ? '106064' : "106063";
+var rewardVideoSpaceId = Platform.isAndroid ? '111366': "111367";
+var timeOut = 5000;//广告请求超时时长，建议5000毫秒,该参数单位为ms
+
+
